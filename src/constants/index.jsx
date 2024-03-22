3bfddbec101e4189534f3e43ff749ba92ae4804a
CinnamonfirJ <111068472+CinnamonfirJ@users.noreply.github.com>
2024-03-22 22:40:01 +0100

    Add files via upload

---
+++ src/constants/index.jsx
@@ -0,0 +1,332 @@
+export const questions = [
+  {
+    id: 1,
+    question: "One of the following is a derived unit",
+    options: [
+      { id: 0, text: "Ampere", isCorrect: false },
+      { id: 1, text: "Kilogram", isCorrect: false },
+      { id: 2, text: "Ohm", isCorrect: true },
+      { id: 3, text: "Kelvin", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    question:
+      "One of the equations of linear motion is given by V^2 = U^2 + 2as. What is the dimension of the term V^2 – U^2",
+    options: [
+      { id: 0, text: "MLT^-1", isCorrect: false },
+      { id: 1, text: "L^2T^2", isCorrect: true },
+      { id: 2, text: "L^-1T^-1", isCorrect: false },
+      { id: 3, text: "L^2T^-2", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    question: "which of the following is equivalent to 12.5Å",
+    options: [
+      { id: 0, text: "1.25x10^10m", isCorrect: false },
+      { id: 1, text: "1.25nm", isCorrect: true },
+      { id: 2, text: "12500µm", isCorrect: false },
+      { id: 3, text: "1.25x10^-10m", isCorrect: false },
+    ],
+  },
+  {
+    id: 4,
+    question: "Which of the following is not correct",
+    options: [
+      { id: 0, text: "10 ms^-1 = 36 kmh^-1", isCorrect: false },
+      { id: 1, text: "22 gmm^-3 = 22 x 10^6 kgm^-3", isCorrect: false },
+      { id: 2, text: "1 volt = 1 kgm^2S^-3A^-1", isCorrect: true },
+      { id: 3, text: "None of these", isCorrect: false },
+    ],
+  },
+  {
+    id: 5,
+    question: "Which is the correct dimension for power?",
+    options: [
+      { id: 0, text: "ML^2T^-3", isCorrect: true },
+      { id: 1, text: "ML^2T^-2", isCorrect: false },
+      { id: 2, text: "ML^2T^3", isCorrect: false },
+      { id: 3, text: "ML^2T^2", isCorrect: false },
+    ],
+  },
+  {
+    id: 6,
+    question:
+      "For what values of x, y and z is the expression T= kMxLygz is the period of a simple pendulum? Where k is constant.",
+    options: [
+      { id: 0, text: "x=1, y=1, z=1", isCorrect: false },
+      { id: 1, text: "x=0, y=1, z=1/2", isCorrect: false },
+      { id: 2, text: "x=0, y=1/2 z= -1/2", isCorrect: true },
+      { id: 3, text: "x=1, y=1/2, z=1/2", isCorrect: false },
+    ],
+  },
+  {
+    id: 7,
+    question:
+      "Given three vectors r1 = 3i – 2j + k, r2 = 2i-4j-3k and r3 = -I + 2j +2k. the magnitude of their resultant is …",
+    options: [
+      { id: 0, text: "4.85", isCorrect: false },
+      { id: 1, text: "32", isCorrect: false },
+      { id: 2, text: "5.66", isCorrect: true },
+      { id: 3, text: "0", isCorrect: false },
+    ],
+  },
+  {
+    id: 8,
+    question:
+      "An object falls from rest. Find the distance travelled at 8th second of its motion.",
+    options: [
+      { id: 0, text: "45m", isCorrect: false },
+      { id: 1, text: "75m", isCorrect: true },
+      { id: 2, text: "320m", isCorrect: false },
+      { id: 3, text: "245m", isCorrect: false },
+    ],
+  },
+  {
+    id: 9,
+    question: "The working of a rocket is based on;",
+    options: [
+      { id: 0, text: "Newton’s first law", isCorrect: false },
+      { id: 1, text: "Newton’s second law", isCorrect: false },
+      { id: 2, text: "Newton’s third law", isCorrect: true },
+      {
+        id: 3,
+        text: "Newton’s first and Newton’s third law",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question:
+      "Which of the following statement is/are true about Newton’s first law of motion? \ni. It is sometimes called law of inertia \nii. An object moving with constant velocity is in equilibrium  \niii. An object that is changing its velocity has a resultant force on it \niv. An object that has uniform velocity has a resultant force on it",
+    options: [
+      { id: 0, text: "i, ii and iii only", isCorrect: true },
+      {
+        id: 1,
+        text: "ii, iii and iv only",
+        isCorrect: false,
+      },
+      {
+        id: 2,
+        text: "i, iii and iv only",
+        isCorrect: false,
+      },
+      {
+        id: 3,
+        text: "i, ii and iv only",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question:
+      "A body of mass m is moving in a straight line at constant speed V. Its kinetic energy is k and magnitude of momentum is P. which of the following relation is correct?",
+    options: [
+      { id: 0, text: "P = 2mk", isCorrect: false },
+      { id: 1, text: "P = k^2/2m", isCorrect: false },
+      { id: 2, text: "2k = PV", isCorrect: true },
+      { id: 3, text: "V = 2k/p", isCorrect: false },
+    ],
+  },
+  {
+    id: 12,
+    question:
+      "The mass of a body accelerating at 12.5m/s^2 with a force of 100N is",
+    options: [
+      { id: 0, text: "0.8kg", isCorrect: false },
+      { id: 1, text: "8kg", isCorrect: true },
+      { id: 2, text: "80kg", isCorrect: false },
+      { id: 3, text: "800kg", isCorrect: false },
+    ],
+  },
+  {
+    id: 13,
+    question:
+      "Two particles of masses m and 4m have kinetic energies in the ratio 2:1 what is the ratio of their linear momentum.",
+    options: [
+      { id: 0, text: "1/2", isCorrect: false },
+      { id: 1, text: "1/√2", isCorrect: false },
+      { id: 2, text: "1/4", isCorrect: true },
+      { id: 3, text: "1/6", isCorrect: false },
+    ],
+  },
+  {
+    id: 14,
+    question:
+      "A boy of mass 5000g jumps onto the ground from 200cm high. Calculate the force on him when he landed and bends his knee to stop in 0.5seconds, (g = 10m/s^2).",
+    options: [
+      { id: 0, text: "4472N", isCorrect: false },
+      { id: 1, text: "44720N", isCorrect: false },
+      { id: 2, text: "63N", isCorrect: true },
+      { id: 3, text: "632.46N", isCorrect: false },
+    ],
+  },
+  {
+    id: 15,
+    question:
+      "Two bodies of masses m1 and m2 have equal kinetic energies. If P1 and P2 are their respective momentum, what is the ratio of P2:P1",
+    options: [
+      { id: 0, text: "m2 : m1", isCorrect: false },
+      { id: 1, text: "√(m_2 ):√m_1", isCorrect: true },
+      { id: 2, text: "2m2 : 2m1", isCorrect: false },
+      { id: 3, text: "m_22 ∶m_12", isCorrect: false },
+    ],
+  },
+  {
+    id: 16,
+    question:
+      "A body of mass 1Kg is thrown vertically upwards with a velocity of 20m/s. it momentarily comes to rest at a height of 18m. how much energy is lost due to air friction.",
+    options: [
+      { id: 0, text: "20J", isCorrect: false },
+      { id: 1, text: "30J", isCorrect: false },
+      { id: 2, text: "400J", isCorrect: false },
+      { id: 3, text: "180J", isCorrect: true },
+    ],
+  },
+  {
+    id: 17,
+    question:
+      "Which of the following is correct when a mango falls from a tree",
+    options: [
+      { id: 0, text: "Only the mango attracts the earth", isCorrect: false },
+      { id: 1, text: "Only the earth attract the mango", isCorrect: false },
+      {
+        id: 2,
+        text: "The attraction by the earth is much greater than that of the mango",
+        isCorrect: false,
+      },
+      {
+        id: 3,
+        text: "Both the earth and the mango attract each other",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    id: 18,
+    question:
+      "If R is the radius of the earth and ME is the mass of the earth, the gravitational field, g, of the earth is",
+    options: [
+      { id: 0, text: "(GM_E m)/R^2", isCorrect: false },
+      { id: 1, text: "(GM_E)/R", isCorrect: false },
+      { id: 2, text: "(GM_E)/R^2", isCorrect: true },
+      { id: 3, text: "(GR^2)/M_E", isCorrect: false },
+    ],
+  },
+  {
+    id: 19,
+    question:
+      "Which of the following is/are correct \n(i) The speed of the satellite depends on the radius of the orbit \n(ii) The total mechanical energy in a circular orbit is equal to half potential energy \n(iii) Parking orbit is the orbit which has the same period as the earth \n(iv) The period of the satellite is proportional to the velocity of the satellite",
+    options: [
+      {
+        id: 0,
+        text: "(ii) only",
+        isCorrect: false,
+      },
+      {
+        id: 1,
+        text: "(iii) only",
+        isCorrect: false,
+      },
+      {
+        id: 2,
+        text: " (i), (ii) and (iv) only",
+        isCorrect: false,
+      },
+      {
+        id: 3,
+        text: " (i), (ii) and (iii) only",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    id: 20,
+    question:
+      "Two balls of 50kg each are placed so that their centers are 50cm apart. What is the magnitude of gravitational force between the two balls, (6.67x10^-11Nm^2kg^-2)?",
+    options: [
+      { id: 0, text: "6.67x10^-11N", isCorrect: false },
+      { id: 1, text: "6.67x10^-10N", isCorrect: false },
+      { id: 2, text: "6.67x10^-9N", isCorrect: false },
+      { id: 3, text: "6.67x10^-7N", isCorrect: true },
+    ],
+  },
+  {
+    id: 21,
+    question: "If RE is the radius of the earth, the escape velocity is",
+    options: [
+      { id: 0, text: "(GM_E)/R_E", isCorrect: false },
+      { id: 1, text: "2GrE", isCorrect: false },
+      { id: 2, text: "√(2gR_E)", isCorrect: true },
+    ],
+  },
+  {
+    id: 22,
+    question:
+      "If an object is projected at an angle of 45° to the horizontal, then",
+    options: [
+      { id: 0, text: "maximum height is obtained", isCorrect: false },
+      { id: 1, text: "maximum range is obtained", isCorrect: true },
+      { id: 2, text: "maximum time is obtained", isCorrect: false },
+      { id: 3, text: "minimum height is obtained", isCorrect: false },
+    ],
+  },
+  {
+    id: 23,
+    question:
+      "An object is projected with a velocity 10𝑚𝑠−1 at an angle of 60° with the surface of the ground. The time taken to reach the maximum height is",
+    options: [
+      { id: 0, text: "0.87 seconds", isCorrect: true },
+      { id: 1, text: "8.7 seconds", isCorrect: false },
+      { id: 2, text: "0.67 seconds", isCorrect: false },
+      { id: 3, text: "67 seconds", isCorrect: false },
+    ],
+  },
+  {
+    id: 24,
+    question:
+      "A ball is projected horizontally from the top of a vertical cliff 50m high with a velocity of 25𝑚𝑠−1. The time taken by the ball to reach the surface of the ground is",
+    options: [
+      { id: 0, text: "2.2 seconds", isCorrect: false },
+      { id: 1, text: "3.2 seconds", isCorrect: true },
+      { id: 2, text: "7.1 seconds", isCorrect: false },
+      { id: 3, text: "10 seconds", isCorrect: false },
+    ],
+  },
+  {
+    id: 25,
+    question:
+      "A particle having an initial velocity of 5𝑚𝑠−1 moves along a straight line with a constant acceleration of 2𝑚𝑠−2 for 10𝑠. Its displacement in the last second is",
+    options: [
+      { id: 0, text: "25m", isCorrect: false },
+      { id: 1, text: "30m", isCorrect: true },
+      { id: 2, text: "24m", isCorrect: false },
+      { id: 3, text: "27m", isCorrect: false },
+    ],
+  },
+  {
+    id: 26,
+    question:
+      "A particle starts moving from rest under constant acceleration. It travels a distance 𝑥 in the first 10𝑠 and distance 𝑦 in the next 10𝑠, then",
+    options: [
+      { id: 0, text: "𝑦 = 𝑥", isCorrect: false },
+      { id: 1, text: "𝑦 = 2𝑥", isCorrect: true },
+      { id: 2, text: "𝑦 = 3𝑥", isCorrect: false },
+      { id: 3, text: "𝑦 = 4𝑥", isCorrect: false },
+    ],
+  },
+  {
+    id: 27,
+    question:
+      "A stone is dropped from the top of a tower and reached the ground in 3𝑠. Then the height of the tower is (𝑔 = 9.81𝑚𝑠−2)",
+    options: [
+      { id: 0, text: "18.6m", isCorrect: false },
+      { id: 1, text: "39.2m", isCorrect: false },
+      { id: 2, text: "44.1m", isCorrect: true },
+      { id: 3, text: "98m", isCorrect: false },
+    ],
+  },
+];
